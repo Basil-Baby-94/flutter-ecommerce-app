3da5b28a2a3def17b3d7e8c7c366745c0b175ce0
Basil Baby <basilbaby94@gmail.com>
2024-09-04 11:59:32 +0200

    App structure created

---
+++ lib/core/constants/endpoints.dart
@@ -0,0 +1,7 @@
+// lib/core/constants/endpoints.dart
+
+class Endpoints {
+  static const String baseUrl = 'https://fakestoreapi.com';
+  static const String getProducts = '$baseUrl/products';
+  static const String getProductDetails = '$baseUrl/products/';
+}
---
+++ lib/core/constants/text_styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  static const TextStyle headline1 = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+
+  static const TextStyle bodyText1 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    color: Colors.grey,
+  );
+
+  static const TextStyle buttonText = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+  );
+}
