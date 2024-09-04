d1362ca1286afd7a56d1ee5fd40c622cd80bf89c
Basil Baby <basilbaby94@gmail.com>
2024-09-04 15:01:47 +0200

    Minor fix

--- lib/presentation/pages/product_list.dart
+++ lib/presentation/pages/product_list.dart
@@ -12,51 +12,53 @@ class ProductListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text(AppConstants.kProduct)),
-      body: BlocBuilder<ProductBloc, ProductState>(
-        builder: (context, state) {
-          if (state is ProductLoading) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (state is ProductLoaded) {
-            return Column(
-              children: [
-                // Top padding and alignment
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 16.0, horizontal: 16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Spacer(),
-                      CategoryPicker(
-                        selectedCategory: state.selectedCategory,
-                        categories: state.categories,
-                        onCategorySelected: (category) {
-                          context
-                              .read<ProductBloc>()
-                              .add(SelectCategory(category));
-                        },
-                      ),
-                    ],
+      body: SafeArea(
+        child: BlocBuilder<ProductBloc, ProductState>(
+          builder: (context, state) {
+            if (state is ProductLoading) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (state is ProductLoaded) {
+              return Column(
+                children: [
+                  // Top padding and alignment
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 16.0, horizontal: 16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Spacer(),
+                        CategoryPicker(
+                          selectedCategory: state.selectedCategory,
+                          categories: state.categories,
+                          onCategorySelected: (category) {
+                            context
+                                .read<ProductBloc>()
+                                .add(SelectCategory(category));
+                          },
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                // Expanded list view for products
-                Expanded(
-                  child: ListView.builder(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    itemCount: state.filteredProducts.length,
-                    itemBuilder: (context, index) {
-                      return ProductCard(
-                          product: state.filteredProducts[index]);
-                    },
+                  // Expanded list view for products
+                  Expanded(
+                    child: ListView.builder(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      itemCount: state.filteredProducts.length,
+                      itemBuilder: (context, index) {
+                        return ProductCard(
+                            product: state.filteredProducts[index]);
+                      },
+                    ),
                   ),
-                ),
-              ],
-            );
-          } else if (state is ProductError) {
-            return Center(child: Text('Error: ${state.message}'));
-          }
-          return Container();
-        },
+                ],
+              );
+            } else if (state is ProductError) {
+              return Center(child: Text('Error: ${state.message}'));
+            }
+            return Container();
+          },
+        ),
       ),
     );
   }
